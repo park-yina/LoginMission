c69bc5f99b027a5fa900111256947b5e6ce86e5c
park-yina <111878820+park-yina@users.noreply.github.com>
2024-05-15 21:42:02 +0900

    Update InfoController.java

--- sbb/src/main/java/com/mysite/sbb/info/InfoController.java
+++ sbb/src/main/java/com/mysite/sbb/info/InfoController.java
@@ -1,18 +1,16 @@
 package com.mysite.sbb.info;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import lombok.RequiredArgsConstructor;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -20,56 +18,39 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @Controller
 @RequestMapping("/user/mypage/first")
 public class InfoController {
-
     private final InfoService infoService;
     private final Environment environment;
-
+    
     @PostMapping("/")
     public String submitInfo(InfoDTO infoDto, @RequestParam("file") MultipartFile singleFile) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String loggedInUsername = authentication.getName();
-
         String username = infoDto.getUsername();
         if (!username.equals(loggedInUsername)) {
-
             return "form_errors"; 
         }
-
+        
+        // 파일 이름 생성
+        String originalFileName = singleFile.getOriginalFilename();
+        String fileName = UUID.randomUUID().toString() + "-" + originalFileName;
+        
+        // 파일 저장
+        try {
+            String filePath = environment.getProperty("file.dir") + File.separator + fileName;
+            File dest = new File(filePath);
+            singleFile.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // 파일 저장 중 오류가 발생하면 처리
+            return "form_errors"; // 에러 페이지로 리다이렉트 또는 에러 메시지 표시
+        }
+        
         // 입력된 정보로 새로운 Info 생성
         String address = infoDto.getAddress();
         String nickname = infoDto.getNickname();
         String phoneNumber = infoDto.getPhoneNumber();
-        System.out.println("singleFile : " + singleFile);
         
-        // 2. 저장할 경로 가져오기
-        String fileDir = environment.getProperty("file.dir");
-        System.out.println("fileDir : " + fileDir);
-        String root = fileDir + "/uploadFiles";
-
-        File file = new File(root);
-
-        // 만약 uploadFiles 폴더가 없으면 생성
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-
-        // 업로드할 폴더 설정
-        String originFileName = singleFile.getOriginalFilename();
-        String ext = originFileName.substring(originFileName.lastIndexOf("."));
-        String ranFileName = UUID.randomUUID().toString() + ext;
-
-        File changeFile = new File(root + "/" + ranFileName);
-
-        // 파일 업로드
-        try {
-            singleFile.transferTo(changeFile);
-            System.out.println("파일 업로드 성공");
-        } catch (IllegalStateException | IOException e) {
-            System.out.println("파일 업로드 실패");
-            e.printStackTrace();
-        }
-
-        infoService.create(username, address, nickname, phoneNumber, ranFileName);
+        infoService.create(username, address, nickname, phoneNumber, fileName);
         infoService.changeFirst(username);
 
         return "redirect:/";
